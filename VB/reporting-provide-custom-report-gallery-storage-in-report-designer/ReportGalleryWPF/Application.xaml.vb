f8a0ed60f2b482ddb12ed23ee9311132aefc77b8
Andrey Lepikhov (auto) <andrey.lepikhov@gmail.com>
2019-05-20 10:41:35 +0300

    auto sync for version 2019.1

--- VB/reporting-provide-custom-report-gallery-storage-in-report-designer/ReportGalleryWPF/Application.xaml.vb
+++ VB/reporting-provide-custom-report-gallery-storage-in-report-designer/ReportGalleryWPF/Application.xaml.vb
@@ -1,13 +1,13 @@
 ﻿Imports System.Windows
 
 Namespace ReportGalleryWPF
-	''' <summary>
-	''' Interaction logic for App.xaml
-	''' </summary>
-	Partial Public Class App
-		Inherits Application
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Partial Public Class App
+        Inherits Application
 
-		Shared Sub New()
-		End Sub
-	End Class
+        Shared Sub New()
+        End Sub
+    End Class
 End Namespace
--- VB/reporting-provide-custom-report-gallery-storage-in-report-designer/XpoReportGalleryStorage/ReportGalleryTable.vb
+++ VB/reporting-provide-custom-report-gallery-storage-in-report-designer/XpoReportGalleryStorage/ReportGalleryTable.vb
@@ -2,74 +2,74 @@
 Imports DevExpress.XtraReports.ReportGallery
 
 Namespace XpoReportGalleryStorage
-	Public Class ReportGalleryTable
-		Inherits XPLiteObject
+    Public Class ReportGalleryTable
+        Inherits XPLiteObject
 
-'INSTANT VB NOTE: The field sID was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private sID_Renamed As String
-'INSTANT VB NOTE: The field displayName was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private displayName_Renamed As String
-'INSTANT VB NOTE: The field hash was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private hash_Renamed As Integer
-'INSTANT VB NOTE: The field contentHash was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private contentHash_Renamed As Integer
-		Private galleryItemType As GalleryItemType
 
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
+        Private sID_Renamed As String
 
-		<Key>
-		Public Property ID() As Integer
+        Private displayName_Renamed As String
 
-		Public Property SID() As String
-			Get
-				Return sID_Renamed
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue("SID", sID_Renamed, value)
-			End Set
-		End Property
+        Private hash_Renamed As Integer
 
-		Public Property ItemKind() As String
+        Private contentHash_Renamed As Integer
+        Private galleryItemType As GalleryItemType
 
-		Public Property DisplayName() As String
-			Get
-				Return displayName_Renamed
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue("DisplayName", displayName_Renamed, value)
-			End Set
-		End Property
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
 
-		Public Property Content() As Byte()
+        <Key> _
+        Public Property ID() As Integer
 
-		Public Property Hash() As Integer
-			Get
-				Return hash_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue("Hash", hash_Renamed, value)
-			End Set
-		End Property
+        Public Property SID() As String
+            Get
+                Return sID_Renamed
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("SID", sID_Renamed, value)
+            End Set
+        End Property
 
-		Public Property ContentHash() As Integer
-			Get
-				Return contentHash_Renamed
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue("ContentHash", contentHash_Renamed, value)
-			End Set
-		End Property
+        Public Property ItemKind() As String
 
-		Public Property ItemType() As GalleryItemType
-			Get
-				Return galleryItemType
-			End Get
-			Set(ByVal value As GalleryItemType)
-				SetPropertyValue("ItemType", galleryItemType, value)
-			End Set
-		End Property
+        Public Property DisplayName() As String
+            Get
+                Return displayName_Renamed
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("DisplayName", displayName_Renamed, value)
+            End Set
+        End Property
 
-	End Class
+        Public Property Content() As Byte()
+
+        Public Property Hash() As Integer
+            Get
+                Return hash_Renamed
+            End Get
+            Set(ByVal value As Integer)
+                SetPropertyValue("Hash", hash_Renamed, value)
+            End Set
+        End Property
+
+        Public Property ContentHash() As Integer
+            Get
+                Return contentHash_Renamed
+            End Get
+            Set(ByVal value As Integer)
+                SetPropertyValue("ContentHash", contentHash_Renamed, value)
+            End Set
+        End Property
+
+        Public Property ItemType() As GalleryItemType
+            Get
+                Return galleryItemType
+            End Get
+            Set(ByVal value As GalleryItemType)
+                SetPropertyValue("ItemType", galleryItemType, value)
+            End Set
+        End Property
+
+    End Class
 End Namespace
